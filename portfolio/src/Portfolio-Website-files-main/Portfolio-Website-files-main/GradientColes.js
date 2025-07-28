828113f229fe05bfa232ee6838f1f6473e5c08d2
vjgopinath12 <vjgopinath12@gmail.com>
2025-07-29 00:29:45 +0530

    Remove node_modules from Git tracking

---
+++ portfolio/src/Portfolio-Website-files-main/Portfolio-Website-files-main/GradientColes.js
@@ -0,0 +1,9 @@
+const mainbg = `bg-gradient-to-tr from-[#0235a3] via-[#030a1c] to-[#05174e]`;
+
+const aboutsection = `bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#030a1c] to-[#05174e]`;
+
+const skills = `bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#05174e] to-[#030a1c]`;
+
+const projects = `bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#030a1c] to-[#05174e]`;
+
+const footer = ` bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#05174e] to-[#030a1c]`;
---
+++ portfolio/src/assets/index.js
@@ -0,0 +1,34 @@
+export { default as Mongodb } from "./mongodb.svg";
+export { default as Reactjs } from "./reactjs.svg";
+export { default as Nodejs } from "./nodejs.svg";
+
+export {default as AWS} from "./aws.svg";
+export {default as MySQL} from "./mysql.svg";
+export {default as Portfolio} from "./work/Portfolio.png";
+export {default as Jkart} from "./work/Jkart.png";
+export {default as TodoList} from "./work/TodoList.png";
+export {default as MemCardGame} from "./work/MemCardGame.png"
+export {default as Currency} from "./work/Currency.png"
+export {default as Calc} from "./work/calc.png"
+
+export { default as Redux } from "./redux.svg";
+export { default as Express } from "./express.svg";
+export { default as Nextjs } from "./nextjs.svg";
+export { default as Nextjs2 } from "./nextjs2.svg";
+
+// export { default as Major } from "./major.png";
+
+export { default as CSS3 } from "./css3.svg";
+export { default as HTML5 } from "./html.svg";
+export { default as Javascript } from "./javascript.svg";
+export { default as Python } from "./python.svg";
+
+// export { default as admin } from "./work/admin.png";
+// export { default as food } from "./work/food.png";
+// export { default as food1 } from "./work/food1.jpg";
+// export { default as port1 } from "./work/port1.png";
+// export { default as port2 } from "./work/port2.png";
+// export { default as port3 } from "./work/port3.png";
+// export { default as social } from "./work/social.png";
+
+export { default as Profile } from "./profile.png";
